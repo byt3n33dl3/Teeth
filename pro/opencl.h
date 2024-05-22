67ac36fc95c32a3979e4a67bde89b6d2a551afa9
p3xsouger <151133481+pxcs@users.noreply.github.com>
2024-05-22 20:18:00 +0700

    pro-prop-ssl
    first-basic

---
+++ pro/opencl.h
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2024 The Khronos Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * C licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+#ifndef __OPENCL_H
+#define __OPENCL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "pro_version.h"
+#include "pro_platform.h"
+#include "pro_extend.h"
+#include "cl_platform.h"
+#include "pro.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
