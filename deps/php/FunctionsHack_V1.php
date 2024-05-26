d6af989cafd0c3a86e61a11b12423234ba3ec039
p3xsouger <151133481+pxcs@users.noreply.github.com>
2024-05-26 19:16:46 +0700

    Add files via upload

---
+++ deps/php/FunctionsHack_V1.php
@@ -0,0 +1,5 @@
+// Hack functions are annotated with types.
+function negate(bool $x): bool {
+    return !$x;
+}
+
---
+++ deps/php/PROJECT_LANG_1.hh
@@ -0,0 +1,15 @@
+<<__EntryPoint>>
+// Start of script
+function projectLanguageOne(): void {
+  echo 'Project language file 1';
+  echo 'For: SNU/2D/ProgrammingTools/IDE/Hack';
+  echo 'About: ';
+  echo 'I decided to make Hack the main project language file for this project (SNU / 2D / Programming Tools / IDE / Hack) as this is a Hack IDE, and it needs its main language to be represented here.';
+}
+return projectLanguageOne();
+/* File info
+* File type: Hack source file (*.hh *.hck *.php *.hack)
+* File version: 1 (2022, Friday, September 30th at 4:42 pm PST)
+* Line count (including blank lines and compiler line): 16
+*/
+// End of script
