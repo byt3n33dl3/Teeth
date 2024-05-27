cacd9bf4ff66ca6002a7a40ee535b9752bd5e429
p3xsouger <151133481+pxcs@users.noreply.github.com>
2024-05-27 22:49:40 +0700

    FactoryTest.cs

---
+++ pro/hashid/Utils/FactoryTest.cs
@@ -0,0 +1,36 @@
+﻿using laget.HashId.Util;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Xunit;
+
+namespace laget.HashId.Tests.Util
+{
+    public class HashIdFactoryTest
+    {
+        [Fact]
+        public void ProperlyParsesConfig()
+        {
+            const string defaultHashVersion = "0x";
+            const string hashVersion2 = "1x";
+
+            const string version0XSalt = "qwer";
+            const string version1XSalt = "asdf";
+
+            var config = new Dictionary<string, string>
+            {
+                { "DefaultHashVersion", defaultHashVersion },
+                { $"SaltVersions:{defaultHashVersion}", version0XSalt },
+                { $"SaltVersions:{hashVersion2}", version1XSalt }
+            };
+
+            var options = new ConfigurationBuilder()
+                .AddInMemoryCollection(config)
+                .Build()
+                .Get<HashIdFactoryOptions>();
+
+            Assert.Equal(defaultHashVersion, options.DefaultHashVersion);
+            Assert.Equal(version0XSalt, options.SaltVersions[defaultHashVersion]);
+            Assert.Equal(version1XSalt, options.SaltVersions[hashVersion2]);
+        }
+    }
+}
